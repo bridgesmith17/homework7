1614f3425dd3ff4cbed57367c2c6d60dadb7b9c6
bridgesmith17 <bridgesmith17@gmail.com>
2016-05-15 09:57:53 -0700

    Update forms-demo.js
    deleted extranous functinos

--- express-forms/forms-demo.js
+++ express-forms/forms-demo.js
@@ -27,39 +27,6 @@ app.get('/get-data',function(req,res){
 
 app.post('/post-data',function(req,res){
   var qParams = [];
-  for (var p in req.query){
-  qParams.push({'name':p,'value':req.query[p]})
-  }
-  var context = {};
-  context.dataList = qParams;
-  res.render('post-data', context);
-});
-
-
-app.get('/get-loopback',function(req,res){
-  var qParams = "";
-  for (var p in req.query){
-    qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
-  }
-  qParams = qParams.substring(0,qParams.lastIndexOf(','));
-  qParams += '.';
-  var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback', context);
-});
-
-app.get('/get-loopback-improved',function(req,res){
-  var qParams = [];
-  for (var p in req.query){
-    qParams.push({'name':p,'value':req.query[p]})
-  }
-  var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback-improved', context);
-});
-
-app.post('/post-loopback', function(req,res){
-  var qParams = [];
   for (var p in req.body){
     qParams.push({'name':p,'value':req.body[p]})
   }
